ea305b3f8cffeae8ba0c818baa4298d71adbeda8
edison <845292122@qq.com>
2025-01-06 21:32:35 +0800

    新增account+perm

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,11 @@
 import zodRouter from 'koa-zod-router'
-import { UserRoute } from './system/user.route'
-import { RoleRoute } from './system/role.route'
-import { TenantRoute } from './system/tenant.route'
-import { PermissionRoute } from './system/permission.route'
+import { AccountRoute } from './account.route'
+import { PermRoute } from './perm.route'
 
 const router = zodRouter()
 
 // * 系统模块
-UserRoute(router)
-RoleRoute(router)
-TenantRoute(router)
-PermissionRoute(router)
+AccountRoute(router)
+PermRoute(router)
 
 export default router
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
-export * from './user'
+export * from './account'
+export * from './perm'
 
 export type IPage = {
   pageNo: number
